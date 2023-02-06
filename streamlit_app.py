33011ebafd431db97e0ecf8db0778cf6fd355891
SmitNbk <124003631+SmitNbk@users.noreply.github.com>
2023-02-06 11:19:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit
 streamlit.title('My Parents new healthy diner')
 
 streamlit.header('Breakfast  Favouites')
-streamlit.text(🍯'Omega 3 & Blueberry Oatmeal')
+streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
 streamlit.text(🥗'Kale, spinach & rocket smoothie')
 streamlit.text(🐔'Hard-boiled, Free-Range egg')
 streamlit.text(🥑🍞'Avocado Toast') 
